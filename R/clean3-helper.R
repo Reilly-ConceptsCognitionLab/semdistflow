d5c4c59a9b18d96b85625c9209ddad634f2a6c48
Bonnie Zuckerman <tuc50262@temple.edu>
2021-11-16 19:21:17 -0500

    commit documentation and cleaning function

---
+++ R/clean3-helper.R
@@ -0,0 +1,21 @@
+#' @import stringi, tm
+clean3 <- function(x) {
+  x <- gsub("person person", "person", x)
+  x <- gsub("people people", "people", x)
+  x <- gsub("woman person", "woman", x)
+  x <- gsub("man person", "man", x)
+  x <- gsub("person people", "person", x)
+  x <- gsub("man man", "man", x)
+  x <- gsub("woman woman", "woman", x)
+  x <- gsub("(.*)(, and* )(\\w{1,}\\s\\w{1,})", "\\1. \\3", x)
+  x <- gsub("(.*)(, but* )(\\w{1,}\\s\\w{1,})", "\\1. \\3", x)
+  x <- gsub("(.*)(,* because* )(\\w{1,}\\s\\w{1,})", "\\1. \\3", x)
+  x <- gsub("(.*)(,* then* )(\\w{1,}\\s\\w{1,})", "\\1. \\3", x)
+  x <- gsub(",", "", x) # remove commas
+  x <- gsub("([[:punct:]])", "", x) # remove periods
+  #x <- gsub("([[:alpha:]]) ([[:punct:]])", "\\1\\2", x) #omit space between any character and a period
+  #x <- gsub("\\s([[:punct:]])", "", x) #omits floating periods from utterance void of content words
+  #x <- gsub("\\.{2,}", "\\. ", x)
+  x <- tm::stripWhitespace(x)
+  x <- stringi::stri_remove_empty(x)
+}
---
+++ R/clean_allsteps.R
@@ -0,0 +1,22 @@
+#' Clean Raw Text
+#'
+#' Use helper functions to clean raw text
+#'
+#' @param x A dataframe with raw text in a column titled "doc_text".
+#' @return the function \code{clean_df} with return a dataframe of cleaned text documents, their document ID and the raw text.
+#' @examples
+#' clean_df(mydata)
+#' @export
+
+clean_df <- function(x){
+
+  rawdat <- x$doc_text
+  clean1dat <- clean1(rawdat)
+  clean2dat <-clean2(clean1dat)
+  doc_clean<- clean3(clean2dat)
+
+  final <- cbind(x,doc_clean)
+
+  return(as.tibble(final))
+
+}
