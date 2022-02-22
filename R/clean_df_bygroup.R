c516e58ea38bf51f68d3402530802666cf401863
Bonnie Zuckerman <tuc50262@temple.edu>
2022-02-22 12:05:09 -0500

    add new function to applying cleaning to mutiple docs

---
+++ R/clean_df_bygroup.R
@@ -0,0 +1,18 @@
+#' Applies cleaning function to multiple texts
+#'
+#' Uses helper functions to clean raw text
+#'
+#' @param x A dataframe with of mutiple documents denoted in a column titled "doc_id" and raw text in a column titled "doc_text" .
+#' @return the function \code{clean_df_bygroup} with return a dataframe of cleaned text documents, their document ID and the raw text.
+#' @importFrom tibble as_tibble
+#' @export
+
+clean_df_bygroup <- function(datafile){
+
+  output <- testfunction_multi %>%
+    group_by(doc_id) %>%
+    group_modify(~ clean_df_nopronouns(.))
+
+  return(output)
+
+}
--- workingscript.R
+++ workingscript.R
@@ -3,107 +3,13 @@ library(roxygen2)
 library(tidyverse)
 library(stringi)
 load_all()
-
-wiki_model_50 <- read.csv("/Users/bonniezuckerman/Documents/GitHub/AphasiaBank/text_tools/wiki_model.csv", header = T)
-wiki_model_100 <- read.csv("/Users/bonniezuckerman/Documents/GitHub/AphasiaBank/text_tools/wiki_model_100d.csv", header = T)
-replacements_nopronouns <- read.csv("/Users/bonniezuckerman/Desktop/SticksandPoem/db_replacements_pronounsremoved.csv", header = T, encoding = "UTF-8")
-replacements <- read.csv("/Users/bonniezuckerman/Desktop/SticksandPoem/db_replacements.csv", header = T, encoding = "UTF-8")
-
-replacements_nopronouns<- replacements_nopronouns %>%
-  mutate(target = tolower(target))
-
-replacements<- replacements %>%
-  mutate(target = tolower(target))
-
-usethis::use_data(replacements, overwrite = TRUE)
-usethis::use_data(replacements_nopronouns, overwrite = TRUE)
-usethis::use_data(wiki_model_50)
-usethis::use_data(wiki_model_100)
-
-rm(replacements)
-
-paths <- sort(Sys.glob(c("data/*.rda", "data/*.RData")))
-res <- tools::checkRdaFiles(paths)
-
 document()
 
-doc_id <- "folder test"
-doc_text<- "Mr. Netherlands, spain, mary, John maria, he, them, they Ms. Oldestone, their newest culinary interpretation of New Hope's iconic Old Stone Church, has opened at 15 S. Main Street. Next year, the internationally-recognized landmark will celebrate 150 years since its building.
 
-The steak and seafood restaurant celebrates New American cuisine on its menu and salutes church history in its decor. Oldestone also features an authentic jazz and cocktail bar, open seven days a week.
-
-The space was the former home of Marsha Brown's restaurant, which closed during the pandemic; Brown passed the baton to the new owners, including Wilfer Naranjo who had previously worked for her as a food runner. Oldestone will also feature several signature creole dishes from Marsha Brown's recipe collection."
-
-testdata<-as.data.frame(cbind(doc_text,doc_id))
-
-
-#working clean data function
-rawdat <- testdata$doc_text
-clean1dat <- clean1(rawdat)
-clean2dat <-clean2.1(clean1dat)
-doc_clean<- clean3(clean2dat)
-
-final <- cbind(testdata,doc_clean)
-
-return(as_tibble(final))
-
+library(dplyr)
 #working read in function
-testfunction <- readin("/Users/bonniezuckerman/Desktop/ReillyLab/texts")
 testfunction_multi <- readin("/Users/bonniezuckerman/Desktop/ReillyLab/multi_texts/short")
-testfunction_multi$doc_id <- "real"
-testfunction_multi<- rownames_to_column(testfunction_multi)
-
-
-list_testfunction_multi<- testfunction_multi %>%
-  group_split(rowname) %>%
-  setNames(unique(testfunction_multi$rowname))
-
-list_testdata_clean<- lapply(list_testfunction_multi, clean_df_nopronouns)
-list_testdata_clean1<- lapply(list_testfunction_multi, clean1)
-list_testdata_clean2<- lapply(list_testdata_clean1, clean2_nopronouns)
-list_testdata_clean3<- lapply(list_testdata_clean2, clean3)
-
-
-
-rawdat <- x$doc_text
-mytext.clean_df <- bind_rows(list_testdata_clean) #convert to dataframe
-mytext.clean_df <- mytext.clean_df %>%
-  mutate(line_num = as.numeric(line_num)) %>%
-  arrange(line_num)
-
-
-
-
-
-
-
-
-clean2.1dat <-clean2.1(clean1dat)
-testclean <-clean_df_nopronouns(testdata)
-
-doc_clean<- clean3(clean2datnp)
-
-clean1dat_split <- unlist(strsplit(clean1dat," "))
-clean2 <- for (i in seq_along(replacements$target)) {
-    y <- gsub(paste0('\\b',replacements$target[i],'\\b'), paste0(' ', replacements$replacement[i],' '), clean1dat_split)
-    return(y)
-}
-
-
-colnames(replacements_nopronouns)
-
-Targ <- paste(replacements$target, collapse=" ")
-rep <- paste(replacements$replacement, collapse=" ")
-z <- stringi::stri_replace_all_regex(clean1dat, "^" %s+% replacements_nopronouns$target %s+% "$", replacements_nopronouns$replacement, vectorize_all = FALSE)
-a <- stringi::stri_replace_all_regex(clean1dat, "^"  %s+% replacements$target %s+% "$", replacements$replacement, vectorize_all = FALSE)
-
-colnames(replacements)
-
-testdata.clean <- clean_df(testdata)
-testdata.clean$doc_clean
-
-testdata.cleannopro <- clean_df_nopronouns(testdata)
-testdata.cleannopro$doc_clean
+outputtest3 <-clean_df_bygroup(testfunction_multi)
 data("wiki_model")
 data("semdist15")
 
@@ -112,7 +18,8 @@ data("semdist15")
 #tokenize cleaned data (if not lemmatizing)
 library(tidytext)
 library(tidyverse)
-clean_tidy_text<- testdata.clean %>%
+clean_tidy_text<- outputtest3 %>%
+  group_by(doc_id, doc_text) %>%
   unnest_tokens(word, doc_clean)
 #lemmatize cleaned data
 clean_tidy_text$lemma<- textstem::lemmatize_words(clean_tidy_text$word)
@@ -122,13 +29,9 @@ clean_tidy_text$lemma<- textstem::lemmatize_words(clean_tidy_text$word)
 
 test <- rowwise_cosine_simil(targetdf = clean_tidy_text, lookupdb = wiki_model, colname1 = lemma, colname2 = Var1)
 test %>% group_by(doc_id) %>% slice(head(row_number(),5))
-
 test.sem <- rowwise_cosine_simil(targetdf = clean_tidy_text, lookupdb=semdist15, colname1 = lemma, colname2 = word)
-test.euc %>% group_by(doc_id) %>% slice(head(row_number(),5))
+test.sem %>% group_by(doc_id) %>% slice(head(row_number(),5))
 sticks_cosine<- left_join(test, test.sem, by = c("doc_id", "doc_text", "word", "lemma_pair1", "lemma"), suffix=c(".glove",".semdist15"))
-
-sticks_cosine<- merge(test, test.sem, by = 0:5, suffix=c(".glove",".semdist15"), sort=F)
+sticks_cosine %>% group_by(doc_id) %>% slice(head(row_number(),5))
 write.csv(sticks_cosine, file = "sticks_cosine_213.csv")
 
-library()
-browseVignettes("gazer")
