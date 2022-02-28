e003c0dec245091eaf09fa27467adaa8e3d94c23
Bonnie Zuckerman <tuc50262@temple.edu>
2022-02-28 18:55:00 -0500

    add option to replace pronouns to cleanme

--- R/clean_df_bygroup.R
+++ R/clean_df_bygroup.R
@@ -3,17 +3,31 @@
 #' Uses helper functions to clean raw text
 #'
 #' @param x A dataframe with of mutiple documents denoted in a column titled "doc_id" and raw text in a column titled "doc_text" .
-#' @return the function \code{clean_df_bygroup} with return a dataframe of cleaned text documents, their document ID and the raw text.
+#' @return the function \code{cleanme} with return a dataframe of cleaned text documents, their document ID and the raw text.
 #' @importFrom tibble as_tibble
 #' @import stringi
 #' @export
 
-clean_df_bygroup <- function(datafile){
+cleanme <- function(datafile, replace_pronoun = FALSE){
 
+  if (replace_pronoun == FALSE) {
+    message("Performing cleaning retaining pronouns")
   output <- datafile %>%
     group_by(doc_id) %>%
     group_modify(~ clean_df_nopronouns(.))
 
   return(output)
+
+  }
+
+  if (replace_pronoun == TRUE) {
+    message("Performing cleaning replacing pronouns")
+    output <- datafile %>%
+      group_by(doc_id) %>%
+      group_modify(~ clean_df(.))
+
+    return(output)
+
+}
 
 }
