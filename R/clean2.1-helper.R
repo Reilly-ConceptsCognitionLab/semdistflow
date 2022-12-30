cb920cc02b895110a55d676bbc593fb68fe26a28
reilly-lab <jamie.reilly@gmail.com>
2022-12-30 11:02:14 -0500

    Updated all functions, deleted old helper files. Added text for the vignette

--- R/clean2.1-helper.R
+++
@@ -1,6 +0,0 @@
-clean2.1 <- function(x) {
-  y <- unlist(strsplit(x, " "))
-  z <- stringi::stri_replace_all_regex(y, "^" %s+% db_replacements$target %s+% "$", db_replacements$replacement, vectorize_all = FALSE)
-  a <- paste(z,collapse=" ")
-  return(a)
-}
---
+++ R/cleanme_23.R
@@ -0,0 +1,18 @@
+#' Clean Raw Text
+#'
+#' Use helper function to clean raw text
+#'
+#' @param x A dataframe with raw text in a column titled "doc_text".
+#' @return the function \code{cleanme} with return a dataframe of cleaned text documents, their document ID and the raw text.
+#' @importFrom tibble as_tibble
+#' @keywords internal
+
+cleanme <- function(dat){
+  message("Performing text cleaning.... sit tight!")
+  doc_clean <- dat %>% group_by(doc_id) %>% dat$doc_text %>% dplyr::group_modify(~ cleantxt(.))
+  #isolate the text column apply the cleantxt function by doc_id
+  final <- cbind(x,doc_clean) #add doc_clean column to original dataframe
+  return(as_tibble(final))
+}
+
+
